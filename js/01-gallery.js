08de9ad6e3c43a8eb59ac844eac84fd9b43a6910
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-03-05 22:39:38 +0200

    completes task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function makeGalleryMarkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}
+
+const galleryItemsMarkup = galleryItems
+  .map((item) => makeGalleryMarkup(item))
+  .join("");
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+galleryEl.addEventListener("click", handleGalleryElClick);
+
+function handleGalleryElClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalUrl = e.target.dataset.source;
+
+  document.addEventListener("keydown", handleEscapeBtn);
+
+  const modal = basicLightbox.create(
+    `<img src="${originalUrl}" alt="${e.target.alt}">`,
+    {
+      onClose: (modal) => {
+        document.removeEventListener("keydown", handleEscapeBtn);
+      },
+    }
+  );
+
+  modal.show();
+
+  function handleEscapeBtn(e) {
+    e.preventDefault();
+    const visible = basicLightbox.visible();
+
+    if (visible && e.code === "Escape") {
+      modal.close();
+    }
+  }
+}
