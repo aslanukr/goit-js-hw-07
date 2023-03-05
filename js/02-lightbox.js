ff33126987fb4c4db22712f94d2b55317273ad5e
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-03-05 23:28:23 +0200

    completes task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function makeGalleryMarkup({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+const galleryItemsMarkup = galleryItems
+  .map((item) => makeGalleryMarkup(item))
+  .join("");
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
